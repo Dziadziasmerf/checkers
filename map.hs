38bb120944c4319a6f13e75d3c10c721b286146d
Szymon Roman <dziadziasmerf@gmail.com>
2016-05-11 13:58:01 +0200

    Mozliwe ruchy

--- map.hs
+++ map.hs
@@ -1,7 +1,7 @@
 import System.IO
 import Data.Char
 initialBoardStr = ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
-data Figure = W | B | WL | BL | E
+data Figure = W | B | WL | BL | E deriving Eq
 type Board = [[Figure]]
 
 
@@ -24,6 +24,7 @@ instance Show Figure where
   show BL = "B"
   show E = "."
 
+
 writeToFile = do
   file <- openFile "map.txt" WriteMode
   putStrLn "Podaj mape:"
@@ -41,13 +42,17 @@ stringToBoard :: String -> Board
 stringToBoard stringBoard = map(map charRep)(lines stringBoard)
 
 printBoard mapa = do
-  putStrLn(show (mapa !! 0))
-  putStrLn(show (mapa !! 1))
-  putStrLn(show (mapa !! 2))
-  putStrLn(show (mapa !! 3))
-  putStrLn(show (mapa !! 4))
-  putStrLn(show (mapa !! 5))
-  putStrLn(show (mapa !! 6))
-  putStrLn(show (mapa !! 7))
+  putStrLn("8"++(show (mapa !! 0)))
+  putStrLn("7"++(show (mapa !! 1)))
+  putStrLn("6"++(show (mapa !! 2)))
+  putStrLn("5"++(show (mapa !! 3)))
+  putStrLn("4"++(show (mapa !! 4)))
+  putStrLn("3"++(show (mapa !! 5)))
+  putStrLn("2"++(show (mapa !! 6)))
+  putStrLn("1"++(show (mapa !! 7)))
+  putStrLn("  A|B|C|D|E|F|G|H")
 
+avaMoves :: Int -> Int -> Board -> [(Int,Int)]
+avaMoves row col map =
+  [(a,b)| a<-[0..7], b<-[0..7], and [abs(a-row)==1,abs(b-col)==1,((map !! a)!!b)==E]]
 
